0e0db5f9039b9bd6ccb0628a1c8ebf85353b8190
Woskus <woskus20@gmail.com>
2024-12-27 17:12:14 +0100

    Improve hover on projects

--- components/projects.tsx
+++ components/projects.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import {Container} from '@/components/container';
 
 interface Project {
@@ -7,44 +8,52 @@ interface Project {
 }
 
 const PROJECTS: Project[] = [
-  {
-    title: 'Strona "Mobilne Rozgrywki Galaxy"',
-    company: 'Samsung',
-    img: '/img/projects/samsung.png',
-  },
-  {
-    title: 'Strona dla PKO BP Fortnite',
-    company: 'PKO BP',
-    img: '/img/projects/pkobp.png',
-  },
-  {
-    title: 'Wordpress to Next.js Migration',
-    company: 'inStreamly',
-    img: '/img/projects/instreamly.png',
-  }
-]
+	{
+		title: 'Strona "Mobilne Rozgrywki Galaxy"',
+		company: 'Samsung',
+		img: '/img/projects/samsung.png',
+	},
+	{
+		title: 'Strona dla PKO BP Fortnite',
+		company: 'PKO BP',
+		img: '/img/projects/pkobp.png',
+	},
+	{
+		title: 'Wordpress to Next.js Migration',
+		company: 'inStreamly',
+		img: '/img/projects/instreamly.png',
+	},
+];
 
 export function Projects() {
-  return (
-    <section className="mt-8">
-      <Container>
-        <h2 className="text-base">Wyróżnione projekty</h2>
+	return (
+		<section className="mt-8">
+			<Container>
+				<h2 className="text-base">Wyróżnione projekty</h2>
 
-        <div className="grid grid-cols-3 gap-2 mt-5">
-          {PROJECTS.map(({title, company, img}) => (
-            <div key={title} className="relative rounded-lg overflow-hidden">
-              <img src={img} alt={title} className="w-full" />
+				<div className="mt-5 grid grid-cols-3 gap-2">
+					{PROJECTS.map(({title, company, img}) => (
+						<Link
+							href="#"
+							key={title}
+							className="group relative cursor-pointer overflow-hidden rounded-lg"
+						>
+							<img
+								src={img}
+								alt={title}
+								className="w-full transition-transform duration-300 group-hover:scale-105"
+							/>
 
-              <div className="bg-gradient-to-t from-black/80 to-black/0 bg-transparent absolute bottom-0 left-0 w-full h-[150px]" />
+							<div className="absolute bottom-0 left-0 h-[150px] w-full bg-transparent bg-gradient-to-t from-black/80 to-black/0" />
 
-              <div className="absolute bottom-5 left-5">
-                <h3 className="text-lg">{title}</h3>
-                <p className="text-muted-foreground text-sm">{company}</p>
-              </div>
-            </div>
-          ))}
-        </div>
-      </Container>
-    </section>
-  );
+							<div className="absolute bottom-5 left-5">
+								<h3 className="text-lg">{title}</h3>
+								<p className="text-sm text-muted-foreground">{company}</p>
+							</div>
+						</Link>
+					))}
+				</div>
+			</Container>
+		</section>
+	);
 }
