4ad0cc5c356043264af683819cd7d9e82c4aa415
Woskus <woskus20@gmail.com>
2024-12-30 13:24:30 +0100

    Fix dark mode icon glitch

--- components/dark-mode-toggler.tsx
+++ components/dark-mode-toggler.tsx
@@ -3,7 +3,6 @@
 import {useTheme} from 'next-themes';
 import {Moon, Sun} from 'lucide-react';
 import {Button} from '@/components/ui/button';
-import {cn} from '@/lib/utils';
 
 export function DarkModeToggler() {
 	const {setTheme, theme} = useTheme();
@@ -13,12 +12,11 @@ export function DarkModeToggler() {
 			variant="ghost"
 			onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}
       size="icon"
-			className={cn(theme ? 'opacity-100' : 'opacity-0')}
 		>
-			{theme === 'dark' ? (
-				<Sun />
-			) : (
+			{theme === 'light' ? (
 				<Moon />
+			) : (
+				<Sun />
 			)}
 		</Button>
 	);
