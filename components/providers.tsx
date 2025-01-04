ddff9b3c51149d4a8cfdae797778ffdf6482a71c
Woskus <woskus20@gmail.com>
2025-01-04 15:52:24 +0100

    Add posthog

--- components/providers.tsx
+++ components/providers.tsx
@@ -1,18 +1,58 @@
 'use client';
 
-import {PropsWithChildren} from 'react';
+import {useEffect, Suspense, PropsWithChildren} from 'react';
+import { usePathname, useSearchParams } from "next/navigation"
 import {ThemeProvider} from 'next-themes';
+import posthog from 'posthog-js';
+import {PostHogProvider, usePostHog} from 'posthog-js/react';
 import {TooltipProvider} from '@/components/ui/tooltip';
 
 export function Providers({children}: PropsWithChildren) {
+	useEffect(() => {
+		posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
+			api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
+			capture_pageview: false,
+			capture_pageleave: true,
+		});
+	}, []);
+
 	return (
-		<ThemeProvider
-			attribute="class"
-			defaultTheme="dark"
-			enableSystem
-			disableTransitionOnChange
-		>
-			<TooltipProvider>{children}</TooltipProvider>
-		</ThemeProvider>
+		<PostHogProvider client={posthog}>
+			<SuspendedPostHogPageView />
+
+			<ThemeProvider
+				attribute="class"
+				defaultTheme="dark"
+				enableSystem
+				disableTransitionOnChange
+			>
+				<TooltipProvider>{children}</TooltipProvider>
+			</ThemeProvider>
+		</PostHogProvider>
 	);
+}
+
+function PostHogPageView() {
+	const pathname = usePathname()
+	const searchParams = useSearchParams()
+	const posthog = usePostHog()
+
+	useEffect(() => {
+		if (pathname && posthog) {
+			let url = window.origin + pathname
+			if (searchParams.toString()) {
+				url = url + `?${searchParams.toString()}`
+			}
+
+			posthog.capture('$pageview', { '$current_url': url })
+		}
+	}, [pathname, searchParams, posthog])
+
+	return null
+}
+
+function SuspendedPostHogPageView() {
+	return <Suspense fallback={null}>
+		<PostHogPageView />
+	</Suspense>
 }
