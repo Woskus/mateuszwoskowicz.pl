092780f1b84dc8862f631419bf25b17ee7196af4
Woskus <woskus20@gmail.com>
2025-01-07 11:39:43 +0100

    Add og image

--- app/page.tsx
+++ app/page.tsx
@@ -14,6 +14,16 @@ export const metadata: Metadata = {
 	title: 'Mateusz Woskowicz - Senior Frontend Developer',
 	description:
 		'Senior Frontend Developer z 5-letnim doświadczeniem, specjalizujący się w React.js, Next.js, TypeScript i rozwiązaniach AI. Obecnie pracuję w inStreamly oraz prowadzący agencję mwebs.',
+	openGraph: {
+		images: [
+			{
+				url: 'https://s3.eu-central-1.amazonaws.com/mateuszwoskowicz.pl/og-image.jpg',
+				width: 1200,
+				height: 628,
+				alt: 'Mateusz Woskowicz - Senior Frontend Developer',
+			},
+		],
+	}
 };
 
 export default function Home() {
