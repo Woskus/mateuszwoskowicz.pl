c6616e52f35ebc2240e3b9884f9eeac5fb5d1b01
Woskus <woskus20@gmail.com>
2025-04-05 08:51:51 +0200

    Cta translations

--- components/cta.tsx
+++ components/cta.tsx
@@ -1,28 +1,30 @@
-import {Download, Linkedin} from 'lucide-react';
+import Link from 'next/link';
+import {useTranslations} from 'next-intl';
+import {Linkedin} from 'lucide-react';
 import {Container} from '@/components/container';
 import {Button} from '@/components/ui/button';
 import {
 	Card,
-	CardContent,
 	CardHeader,
 	CardTitle,
 	CardDescription,
 	CardFooter,
 } from '@/components/ui/card';
-import Link from 'next/link';
 
 export function Cta() {
+	const t = useTranslations('HomePage.cta');
+
 	return (
 		<section className="mt-16">
 			<Container>
-				<Card className="py-8 bg-[url('https://s3.eu-central-1.amazonaws.com/mateuszwoskowicz.pl/cta-background.jpg')] bg-cover bg-no-repeat">
+				<Card className="py-8 dark:bg-[url('https://s3.eu-central-1.amazonaws.com/mateuszwoskowicz.pl/cta-background.jpg')] bg-cover bg-no-repeat">
 					<CardHeader>
 						<CardTitle className="w-full text-center">
-							Chcesz ze mną współpracować?
+							{t('title')}
 						</CardTitle>
 
 						<CardDescription className="text-center text-base">
-							Napisz do mnie na linkedin i bądźmy w kontakcie!
+							{t('description')}
 						</CardDescription>
 					</CardHeader>
 
@@ -33,7 +35,7 @@ export function Cta() {
 								target="_blank"
 							>
 								<Linkedin className="h-4 w-4" />
-								Napisz na Linkedin
+								{t('button')}
 							</Link>
 						</Button>
 					</CardFooter>
